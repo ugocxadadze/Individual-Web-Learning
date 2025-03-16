b642275015c18051cf8744b27f76b66b0de683b4
ugito1 <ugoliza@gmail.com>
2025-03-16 11:36:35 -0400

    goa course

---
+++ day 12/classwork/classwork1.py
@@ -0,0 +1,7 @@
+sport= ["ჩოგბურთი" , "პინკ პონკი" , "ბეისბოლი" , "გოლფი" , "კალათბურთი" ]
+sport.reverse()
+print(sport)
+sport.append("ფეხბურთი")
+print(sport)
+sport.remove( "გოლფი")
+print(sport)
